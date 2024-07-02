0bee5f6fc782fb730af90826c626779d53967588
Linet-Wangui <linetwangui04@gmail.com>
2024-07-02 14:17:33 +0300

    Reset commit

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Mcp0mjM8OuA2RJWc6MBrAQy07bgK5f8tpBpI96xiIM0=\"\n}"
--- more/src/app/page.js
+++
@@ -1,8 +0,0 @@
-import Header from "@/components/layout/Header";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>Home</div>
-  );
-}
